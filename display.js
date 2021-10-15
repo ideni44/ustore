8dfd388830a48a91875645791a11d047ce7fd0ce
denisov <thedenike@gmail.com>
2021-10-15 12:23:20 +0300

    last commit

--- display.js
+++ display.js
@@ -4,22 +4,39 @@ const display = (products, element, filter) => {
   // display products
   element.innerHTML = products
     .map((product) => {
-      const { id, title, img, price } = product;
-      return ` <article class="product">
-          <div class="product-container">
-            <img src="${img}" class="product-img img" alt="${title}" />
-           
-            <div class="product-icons">
-              <button class="product-cart-btn product-icon" data-id="${id}">
-                <i class="fas fa-shopping-cart"></i>
-              </button>
-            </div>
+      const { id, title, img, price, available } = product;
+      if(available){
+        return ` <article class="product">
+        <div class="product-container">
+          <img src="${img}" class="product-img img" alt="${title}" />
+         
+          <div class="product-icons">
+            <button class="product-cart-btn product-icon" data-id="${id}">
+              <i class="fas fa-shopping-cart"></i>
+            </button>
           </div>
-          <footer>
-            <p class="product-name">${title}</p>
-            <h4 class="product-price">${price}$</h4>
-          </footer>
-        </article> `;
+        </div>
+        <footer>
+          <p class="product-name">${title}</p>
+          <h4 class="product-price">${price}$</h4>
+        </footer>
+      </article> `;
+      }
+      else{
+        return ` <article class="product">
+        <div class="product-container">
+          <img src="${img}" class="product-img img" alt="${title}" />
+         
+          <div class="product-icons">
+          <span style="background:black;color:white">Can't add</span>
+          </div>
+        </div>
+        <footer>
+          <p class="product-name">${title}</p>
+          <h4 class="product-price">${price}$</h4>
+        </footer>
+      </article> `;
+      }
     })
     .join('');
     if(filter)return
